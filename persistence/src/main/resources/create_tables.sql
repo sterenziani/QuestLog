cc6e4a04e67da8f57118b15f18c346a5e083b10b
sterenziani <sterenziani@itba.edu.ar>
2020-04-13 22:34:57 -0300

    Remove unnecessary table creation
    This commit comments the creation of certain tables that are not in use in this current version to ensure backwards compatibility in case any of these tables need to be modified before being used in an official release.

--- persistence/src/main/resources/create_tables.sql
+++ persistence/src/main/resources/create_tables.sql
@@ -46,16 +46,16 @@ CREATE TABLE IF NOT EXISTS regions(
 	PRIMARY KEY(region)
 );
 
-CREATE TABLE IF NOT EXISTS users(
-	user_id SERIAL,
-	username VARCHAR(25) UNIQUE NOT NULL,
-	password VARCHAR(50) NOT NULL,
-	email VARCHAR(100) UNIQUE NOT NULL,
-	profile_pic TEXT NOT NULL,
-	region INT NOT NULL,
-	PRIMARY KEY(user_id),
-	FOREIGN KEY(region) REFERENCES regions
-);
+--CREATE TABLE IF NOT EXISTS users(
+--	user_id SERIAL,
+--	username VARCHAR(25) UNIQUE NOT NULL,
+--	password VARCHAR(50) NOT NULL,
+--	email VARCHAR(100) UNIQUE NOT NULL,
+--	profile_pic TEXT NOT NULL,
+--	region INT NOT NULL,
+--	PRIMARY KEY(user_id),
+--	FOREIGN KEY(region) REFERENCES regions
+--);
 
 CREATE TABLE IF NOT EXISTS releases(
 	game INT NOT NULL,
@@ -66,37 +66,37 @@ CREATE TABLE IF NOT EXISTS releases(
 	FOREIGN KEY(region) REFERENCES regions
 );
 
-CREATE TABLE IF NOT EXISTS scores(
-	user_id INT NOT NULL,
-	game INT NOT NULL,
-	score INT NOT NULL,
-	PRIMARY KEY(user_id, game),
-	FOREIGN KEY(user_id) REFERENCES users,
-	FOREIGN KEY(game) REFERENCES games
-);
+--CREATE TABLE IF NOT EXISTS scores(
+--	user_id INT NOT NULL,
+--	game INT NOT NULL,
+--	score INT NOT NULL,
+--	PRIMARY KEY(user_id, game),
+--	FOREIGN KEY(user_id) REFERENCES users,
+--	FOREIGN KEY(game) REFERENCES games
+--);
 
-CREATE TABLE IF NOT EXISTS playstyles(
-	playstyle SERIAL,
-	playstyle_name VARCHAR(25) NOT NULL UNIQUE,
-	PRIMARY KEY(playstyle)
-);
+--CREATE TABLE IF NOT EXISTS playstyles(
+--	playstyle SERIAL,
+--	playstyle_name VARCHAR(25) NOT NULL UNIQUE,
+--	PRIMARY KEY(playstyle)
+--);
 
-CREATE TABLE IF NOT EXISTS reviews(
-	review SERIAL,
-	user_id INT NOT NULL,
-	game INT NOT NULL,
-	platform INT NOT NULL,
-	playstyle INT NOT NULL,
-	time INTERVAL NOT NULL,
-	score INT NOT NULL,
-	body TEXT,
-	post_date TIMESTAMP,
-	PRIMARY KEY(review),
-	FOREIGN KEY(user_id) REFERENCES users,
-	FOREIGN KEY(game) REFERENCES games,
-	FOREIGN KEY(platform) REFERENCES platforms,
-	FOREIGN KEY(playstyle) REFERENCES playstyles
-);
+--CREATE TABLE IF NOT EXISTS reviews(
+--	review SERIAL,
+--	user_id INT NOT NULL,
+--	game INT NOT NULL,
+--	platform INT NOT NULL,
+--	playstyle INT NOT NULL,
+--	time INTERVAL NOT NULL,
+--	score INT NOT NULL,
+--	body TEXT,
+--	post_date TIMESTAMP,
+--	PRIMARY KEY(review),
+--	FOREIGN KEY(user_id) REFERENCES users,
+--	FOREIGN KEY(game) REFERENCES games,
+--	FOREIGN KEY(platform) REFERENCES platforms,
+--	FOREIGN KEY(playstyle) REFERENCES playstyles
+--);
 
 
 CREATE TABLE IF NOT EXISTS genres(
@@ -115,20 +115,20 @@ CREATE TABLE IF NOT EXISTS classifications(
 );
 
 
-CREATE TABLE IF NOT EXISTS lists(
-	list SERIAL,
-	user_id INT NOT NULL,
-	list_name VARCHAR(25) NOT NULL,
-	PRIMARY KEY(list),
-	FOREIGN KEY(user_id) REFERENCES users
-);
+--CREATE TABLE IF NOT EXISTS lists(
+--	list SERIAL,
+--	user_id INT NOT NULL,
+--	list_name VARCHAR(25) NOT NULL,
+--	PRIMARY KEY(list),
+--	FOREIGN KEY(user_id) REFERENCES users
+--);
 
-CREATE TABLE IF NOT EXISTS list_contents(
-	game INT NOT NULL,
-	list INT NOT NULL,
-	date_added DATE NOT NULL,
-	PRIMARY KEY(game, list)
-);
+--CREATE TABLE IF NOT EXISTS list_contents(
+--	game INT NOT NULL,
+--	list INT NOT NULL,
+--	date_added DATE NOT NULL,
+--	PRIMARY KEY(game, list)
+--);
 
 CREATE TABLE IF NOT EXISTS development(
 	game INT NOT NULL,
