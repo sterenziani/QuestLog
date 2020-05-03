5d5f59c301ff7674e6f993a405512d1a93b9ede7
Paula Oseroff <poseroff@itba.edu.ar>
2020-05-03 17:23:05 -0300

    Change Image model to have InputStream instead of byte array

--- model/src/main/java/ar/edu/itba/paw/model/Image.java
+++ model/src/main/java/ar/edu/itba/paw/model/Image.java
@@ -1,24 +1,32 @@
 package ar.edu.itba.paw.model;
 
+import java.io.InputStream;
+
 public class Image {
 
+    private final long      id;
     private final String    imageName;
-    private byte[]          base64CodedImage;
+    private InputStream     base64CodedImage;
 
-    public Image(String imageName, byte[] base64CodedImage){
+    public Image(long id, String imageName, InputStream base64CodedImage){
+        this.id                 = id;
         this.imageName          = imageName;
         this.base64CodedImage   = base64CodedImage;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String   getImageName() {
         return imageName;
     }
 
-    public byte[]   getBase64CodedImage() {
+    public InputStream getBase64CodedImage() {
         return base64CodedImage;
     }
 
-    public void     setBase64CodedImage(byte[] base64CodedImage) {
+    public void setBase64CodedImage(InputStream base64CodedImage) {
         this.base64CodedImage = base64CodedImage;
     }
 }
