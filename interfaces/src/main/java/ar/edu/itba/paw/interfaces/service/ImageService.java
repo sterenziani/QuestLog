66f892d69037df3f242077464976ad5d095788b4
Paula Oseroff <poseroff@itba.edu.ar>
2020-05-03 19:34:37 -0300

    Add ImageService

---
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/ImageService.java
@@ -0,0 +1,25 @@
+package ar.edu.itba.paw.interfaces.service;
+
+import ar.edu.itba.paw.model.Image;
+
+import java.io.InputStream;
+import java.util.Optional;
+
+public interface ImageService {
+    /**
+     * Finds a image given its image_name
+     * @param image_name The unique image_name for the image.
+     * @return The matched image, or null otherwise.
+     */
+
+    Optional<Image> findByImageName(String image_name);
+
+    /**
+     * Uploads an image.
+     * @param image_name	The name of the image.
+     * @param image_data	The InputStream of the image.
+     * @return              The uploaded image.
+     */
+
+    Image           uploadImage(String image_name, InputStream image_data);
+}
