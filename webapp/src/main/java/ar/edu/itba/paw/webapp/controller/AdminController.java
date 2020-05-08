783554ca921d2f69664a1af63a7429ed3e3b904b
Paula Oseroff <poseroff@itba.edu.ar>
2020-05-07 21:50:34 -0300

    Add creation and edition of games mappings. Add to gameForm title, description, genres, platforms, developers, publishers

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AdminController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AdminController.java
@@ -1,6 +1,6 @@
 package ar.edu.itba.paw.webapp.controller;
-import ar.edu.itba.paw.interfaces.service.ImageService;
-import ar.edu.itba.paw.model.Image;
+import ar.edu.itba.paw.interfaces.service.*;
+import ar.edu.itba.paw.model.*;
 import ar.edu.itba.paw.webapp.exception.BadImageException;
 import ar.edu.itba.paw.webapp.form.GameForm;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +15,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class AdminController
 {
 	@Autowired
-	private ImageService is;
+	private ImageService 		is;
+
+	@Autowired
+	private PlatformService 	ps;
+
+	@Autowired
+	private DeveloperService 	ds;
+
+	@Autowired
+	private PublisherService 	pubs;
+
+	@Autowired
+	private GenreService 		gs;
 
 	@RequestMapping("/admin")
 	public ModelAndView admin()
@@ -29,26 +42,65 @@ public class AdminController
 	}
 
 	@RequestMapping(
-			value = "/admin/game",
+			value = "/admin/game/new",
 			method = RequestMethod.GET
 	)
-	public ModelAndView gameEdit(@ModelAttribute("gameForm") final GameForm gameForm){
-		return new ModelAndView("admin/gameForm");
+	public ModelAndView newGame(@ModelAttribute("gameForm") final GameForm gameForm){
+		ModelAndView mav = new ModelAndView("admin/game/gameForm");
+		List<Platform> 	platforms 	= ps.getAllPlatforms();
+		List<Developer> developers 	= ds.getAllDevelopers();
+		List<Publisher> publishers 	= pubs.getAllPublishers();
+		List<Genre> 	genres 		= gs.getAllGenres();
+		mav.addObject("platforms", platforms);
+		mav.addObject("developers", developers);
+		mav.addObject("publishers", publishers);
+		mav.addObject("genres", genres);
+		return mav;
 	}
 
 	@RequestMapping(
-			value = "/admin/game",
+			value = "/admin/game/new",
 			method = RequestMethod.POST
 	)
-	public ModelAndView gameUpdate(@Valid @ModelAttribute("gameForm") final GameForm gameForm, final BindingResult errors, HttpServletRequest request, HttpServletResponse response){
+	public ModelAndView createGame(@Valid @ModelAttribute("gameForm") final GameForm gameForm, final BindingResult errors, HttpServletRequest request, HttpServletResponse response){
 		if(errors.hasErrors()){
-			return gameEdit(gameForm);
+			return newGame(gameForm);
 		}
 		try {
-			final Image image = is.uploadImage(gameForm.getImage().getOriginalFilename(), gameForm.getImage().getBytes());
+			final Image image = is.uploadImage(gameForm.getCover().getOriginalFilename(), gameForm.getCover().getBytes());
 		} catch (IOException e){
 			throw new BadImageException();
 		}
-		return new ModelAndView("admin/gameForm");
+		ModelAndView mav = new ModelAndView("admin/game/gameForm");
+		//mav.addObject("platforms", platforms);
+		return mav;
+	}
+
+	@RequestMapping(
+			value = "/admin/game/{id}/edit",
+			method = RequestMethod.GET
+	)
+	public ModelAndView editGame(@ModelAttribute("gameForm") final GameForm gameForm){
+		ModelAndView mav = new ModelAndView("admin/game/gameForm");
+		//mav.addObject("platforms", platforms);
+		return mav;
+	}
+
+	@RequestMapping(
+			value = "/admin/game/{id}/edit",
+			method = RequestMethod.POST
+	)
+	public ModelAndView updateGame(@Valid @ModelAttribute("gameForm") final GameForm gameForm, final BindingResult errors, HttpServletRequest request, HttpServletResponse response){
+		if(errors.hasErrors()){
+			return editGame(gameForm);
+		}
+		try {
+			final Image image = is.uploadImage(gameForm.getCover().getOriginalFilename(), gameForm.getCover().getBytes());
+		} catch (IOException e){
+			throw new BadImageException();
+		}
+		ModelAndView mav = new ModelAndView("admin/game/gameForm");
+		//mav.addObject("platforms", platforms);
+		return mav;
 	}
 }
