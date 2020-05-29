eaa8959f9e0a0c0f502f7989788c1d5b4ee44dd9
sterenziani <sterenziani@itba.edu.ar>
2020-05-28 23:07:49 -0300

    Add Hibernate configuration for testing

--- persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfig.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfig.java
@@ -1,12 +1,17 @@
 package ar.edu.itba.paw.persistence;
-
+import java.util.Properties;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-
 import org.hsqldb.jdbc.JDBCDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @ComponentScan({"ar.edu.itba.paw.persistence", })
 @Configuration
@@ -15,11 +20,33 @@ public class TestConfig
 	@Bean
 	public DataSource dataSource()
 	{
-		final SimpleDriverDataSource ds = new SimpleDriverDataSource();
-		ds.setDriverClass(JDBCDriver.class);
-		ds.setUrl("jdbc:hsqldb:mem:paw");
-		ds.setUsername("ha");
-        ds.setPassword("");
-        return ds;
-    }
+    	final SimpleDriverDataSource ds = new SimpleDriverDataSource();
+    	ds.setDriverClass(JDBCDriver.class);
+    	ds.setUrl("jdbc:hsqldb:mem:paw");
+    	ds.setUsername("ha");
+    	ds.setPassword("");
+    	return ds;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory()
+	{
+		final LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setPackagesToScan("ar.edu.itba.paw.model");
+		factoryBean.setDataSource(dataSource());
+		final JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		factoryBean.setJpaVendorAdapter(vendorAdapter);
+		final Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update"); // Puede que necesite ser create, pero en ese caso tira excepciones
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+		properties.setProperty("format_sql", "true");
+		factoryBean.setJpaProperties(properties);
+		return factoryBean;
+	}
+    
+	@Bean
+	public PlatformTransactionManager transactionManager(final EntityManagerFactory emf)
+	{
+    	return new JpaTransactionManager(emf);
+	}
 }
