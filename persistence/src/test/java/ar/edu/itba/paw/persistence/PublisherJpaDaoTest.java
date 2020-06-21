0a5d71cc44134caaadc3862063bd8d71ed1a70c7
Paula Oseroff <poseroff@itba.edu.ar>
2020-06-21 18:01:27 -0300

    Update PublisherDaoTest for JPA and JdbcTestUtils testing

--- persistence/src/test/java/ar/edu/itba/paw/persistence/PublisherJpaDaoTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/PublisherJpaDaoTest.java
@@ -1,7 +1,9 @@
-/*package ar.edu.itba.paw.persistence;
+package ar.edu.itba.paw.persistence;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,18 +11,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import ar.edu.itba.paw.model.entity.Publisher;
 import ar.edu.itba.paw.model.entity.Game;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
-@Sql(scripts = {"classpath:schema.sql"})
-public class PublisherJdbcDaoTest
+@Transactional
+public class PublisherJpaDaoTest
 {
 	private static final String GAME_TABLE = "games";
 	private	static final String PUBLISHER_TABLE = "publishers";
@@ -29,27 +30,23 @@ public class PublisherJdbcDaoTest
 	private static final String GAME_TITLE = "Example Game";
 	private static final String GAME_COVER = "http://sega.com/game.jpg";
 	private static final String GAME_DESC = "Explore the world!";
+    private static final String GAME_TRAILER			= "DpHDJRGuL7w";
 	private static final String PUBLISHING_TABLE = "publishing";
 
+	@PersistenceContext
+    private EntityManager em;
 
 	@Autowired
 	private DataSource ds;
 	
 	@Autowired
-	private PublisherJdbcDao publisherDao;
+	private PublisherJpaDao publisherDao;
 	private JdbcTemplate jdbcTemplate;
-	private SimpleJdbcInsert pubInsert;
-	private SimpleJdbcInsert gameInsert;
-	private SimpleJdbcInsert publishingInsert;
 	
 	@Before
 	public void	setUp()
 	{
-		publisherDao = new PublisherJdbcDao(ds);
 		jdbcTemplate = new JdbcTemplate(ds);
-		pubInsert = new SimpleJdbcInsert(ds).withTableName(PUBLISHER_TABLE).usingGeneratedKeyColumns("publisher");
-		gameInsert = new SimpleJdbcInsert(ds).withTableName(GAME_TABLE).usingGeneratedKeyColumns("game");
-		publishingInsert = new SimpleJdbcInsert(ds).withTableName(PUBLISHING_TABLE);
 	}
 	
 	@Test
@@ -75,7 +72,7 @@ public class PublisherJdbcDaoTest
 	public void	testFindPublisherByIdExists()
 	{
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, PUBLISHER_TABLE);
-		Publisher p = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, pubInsert);
+		Publisher p = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, em);
 		Optional<Publisher> maybePublisher = publisherDao.findById(p.getId());
 		Assert.assertTrue(maybePublisher.isPresent());
 		Assert.assertEquals(PUBLISHER_NAME, maybePublisher.get().getName());
@@ -94,7 +91,7 @@ public class PublisherJdbcDaoTest
 	public void	testFindPublisherByNameExists()
 	{
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, PUBLISHER_TABLE);
-		TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, pubInsert);
+		TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, em);
 		Optional<Publisher> maybePublisher = publisherDao.findByName(PUBLISHER_NAME);
 		Assert.assertTrue(maybePublisher.isPresent());
 		Assert.assertEquals(PUBLISHER_NAME, maybePublisher.get().getName());
@@ -105,7 +102,7 @@ public class PublisherJdbcDaoTest
 	public void	testChangePublisherName()
 	{
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, PUBLISHER_TABLE);
-		Publisher p = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, pubInsert);
+		Publisher p = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, em);
 		Optional<Publisher> maybePublisher = publisherDao.changeName(p.getId(), "Noentiendo");
 		Assert.assertTrue(maybePublisher.isPresent());
 		Assert.assertEquals("Noentiendo", maybePublisher.get().getName());
@@ -116,7 +113,7 @@ public class PublisherJdbcDaoTest
 	public void	testChangeLogo()
 	{
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, PUBLISHER_TABLE);
-		Publisher p = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, pubInsert);
+		Publisher p = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, em);
 		Optional<Publisher> maybePublisher = publisherDao.changeLogo(p.getId(), "http://sega.com/logo.png");
 		Assert.assertTrue(maybePublisher.isPresent());
 		Assert.assertEquals(PUBLISHER_NAME, maybePublisher.get().getName());
@@ -129,24 +126,24 @@ public class PublisherJdbcDaoTest
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, PUBLISHER_TABLE);
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, PUBLISHING_TABLE);
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, GAME_TABLE);
-		Publisher nintendo = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, pubInsert);
-		Publisher sega = TestMethods.addPublisher("Sega", "http://sega.com/logo.png", pubInsert);
-		Publisher konami = TestMethods.addPublisher("Konami", PUBLISHER_LOGO, pubInsert);
+		Publisher nintendo = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, em);
+		Publisher sega = TestMethods.addPublisher("Sega", "http://sega.com/logo.png", em);
+		Publisher konami = TestMethods.addPublisher("Konami", PUBLISHER_LOGO, em);
 		List<Publisher> myList = new ArrayList<Publisher>();
 		myList.add(nintendo);
 		myList.add(sega);
 		myList.add(konami);
 		
-		Game g1 = TestMethods.addGame(GAME_TITLE, GAME_COVER, GAME_DESC, gameInsert);
-		TestMethods.connectPub(g1, nintendo, publishingInsert);
-		TestMethods.connectPub(g1, sega, publishingInsert);
+		Game g1 = TestMethods.addGame(GAME_TITLE, GAME_COVER, GAME_DESC, GAME_TRAILER, em);
+		TestMethods.connectPub(g1, nintendo, em);
+		TestMethods.connectPub(g1, sega, em);
 		
 		List<Publisher> publisherList = publisherDao.getAllPublishers();
 		
 		Assert.assertFalse(publisherList.isEmpty());
-		Assert.assertEquals(2, publisherList.size());
-		Assert.assertEquals(publisherList.get(0).getName(), myList.get(0).getName());
-		Assert.assertEquals(publisherList.get(1).getName(), myList.get(1).getName());
+		Assert.assertEquals(3, publisherList.size());
+		Assert.assertTrue(myList.containsAll(publisherList));
+		Assert.assertTrue(publisherList.containsAll(myList));
 	}
 	
 	@Test
@@ -155,18 +152,18 @@ public class PublisherJdbcDaoTest
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, PUBLISHING_TABLE);
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, GAME_TABLE);
 		
-		Publisher konami = TestMethods.addPublisher("Konami", PUBLISHER_LOGO, pubInsert);
-		Publisher nintendo = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, pubInsert);
-		Publisher sega = TestMethods.addPublisher("Sega", PUBLISHER_LOGO, pubInsert);
+		Publisher konami = TestMethods.addPublisher("Konami", PUBLISHER_LOGO, em);
+		Publisher nintendo = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, em);
+		Publisher sega = TestMethods.addPublisher("Sega", PUBLISHER_LOGO, em);
 		List<Publisher> myList = new ArrayList<Publisher>();
 		myList.add(konami);
 		myList.add(nintendo);
 		myList.add(sega);
-		Game g1 = TestMethods.addGame(GAME_TITLE, GAME_COVER, GAME_DESC, gameInsert);
-		Game g2 = TestMethods.addGame("game2", GAME_COVER, GAME_DESC, gameInsert);
-		TestMethods.connectPub(g2, nintendo, publishingInsert);
-		TestMethods.connectPub(g1, konami, publishingInsert);
-		TestMethods.connectPub(g2, konami, publishingInsert);
+		Game g1 = TestMethods.addGame(GAME_TITLE, GAME_COVER, GAME_DESC, GAME_TRAILER, em);
+		Game g2 = TestMethods.addGame("game2", GAME_COVER, GAME_DESC, GAME_TRAILER, em);
+		TestMethods.connectPub(g2, nintendo, em);
+		TestMethods.connectPub(g1, konami, em);
+		TestMethods.connectPub(g2, konami, em);
 		
 		List<Publisher> publisherList1 = publisherDao.getBiggestPublishers(1);
 		List<Publisher> publisherList2 = publisherDao.getBiggestPublishers(3);
@@ -188,9 +185,9 @@ public class PublisherJdbcDaoTest
 	public void testGetPublishers() {
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, PUBLISHER_TABLE);
 		
-		Publisher konami = TestMethods.addPublisher("Konami", PUBLISHER_LOGO, pubInsert);
-		Publisher nintendo = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, pubInsert);
-		Publisher sega = TestMethods.addPublisher("Sega", PUBLISHER_LOGO, pubInsert);
+		Publisher konami = TestMethods.addPublisher("Konami", PUBLISHER_LOGO, em);
+		Publisher nintendo = TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, em);
+		Publisher sega = TestMethods.addPublisher("Sega", PUBLISHER_LOGO, em);
 
 		List<Publisher> myList = new ArrayList<Publisher>();
 		myList.add(konami);
@@ -212,25 +209,22 @@ public class PublisherJdbcDaoTest
 		Assert.assertEquals(3, publisherList3.size());
 
 		
-		Assert.assertEquals(publisherList1.get(0).getName(), myList.get(0).getName());
-		Assert.assertEquals(publisherList1.get(1).getName(), myList.get(1).getName());
+		Assert.assertTrue(myList.subList(0,2).containsAll(publisherList1));
 
-		Assert.assertEquals(publisherList2.get(0).getName(), myList.get(2).getName());
+		Assert.assertTrue(myList.subList(2,3).containsAll(publisherList2));
 
-		Assert.assertEquals(publisherList3.get(0).getName(), myList.get(0).getName());
-		Assert.assertEquals(publisherList3.get(1).getName(), myList.get(1).getName());
-		Assert.assertEquals(publisherList3.get(2).getName(), myList.get(2).getName());
+		Assert.assertTrue(myList.subList(0,3).containsAll(publisherList3));
 	}
 	
 	@Test
 	public void testCountPublishers()
 	{
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, PUBLISHER_TABLE);
-		TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, pubInsert);
-		TestMethods.addPublisher("Sega", PUBLISHER_LOGO, pubInsert);
+		TestMethods.addPublisher(PUBLISHER_NAME, PUBLISHER_LOGO, em);
+		TestMethods.addPublisher("Sega", PUBLISHER_LOGO, em);
 		int count = publisherDao.countPublishers();
 		Assert.assertEquals(2, count);
-		TestMethods.addPublisher("Gamefreak", PUBLISHER_LOGO, pubInsert);
+		TestMethods.addPublisher("Gamefreak", PUBLISHER_LOGO, em);
 		count = publisherDao.countPublishers();
 		Assert.assertEquals(3, count);
 		JdbcTestUtils.deleteFromTables(jdbcTemplate, PUBLISHER_TABLE);
@@ -239,4 +233,3 @@ public class PublisherJdbcDaoTest
 	}
 
 }
-*/
