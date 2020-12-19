116a04e394ddc76622973d8a70b895211295d67b
Paula Oseroff <poseroff@itba.edu.ar>
2020-12-19 18:41:32 -0300

    Create Navigation Component

---
+++ webapp/src/main/webapp/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <React.Fragment>
+
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Navigation;
