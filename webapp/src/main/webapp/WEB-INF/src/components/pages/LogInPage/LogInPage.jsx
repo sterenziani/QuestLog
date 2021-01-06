e9e1cbf5f29273a65b5d8b25bb222db0bfc2b184
Paula Oseroff <poseroff@itba.edu.ar>
2021-01-06 14:52:51 -0300

    Add LogInPage and SignUpPage Components and Navigation

---
+++ webapp/src/main/webapp/WEB-INF/src/components/pages/LogInPage/LogInPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LogInPage extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <p>Hello World!</p>
+        );
+    }
+}
+ 
+export default LogInPage;
