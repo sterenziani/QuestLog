0029e9e51527a90606f6ab2663fb64db9b8beb73
sterenziani <sterenziani@itba.edu.ar>
2021-01-08 02:40:17 -0300

    Finish style for GameListItem

--- webapp/src/main/webapp/WEB-INF/src/components/common/GameListItem/GameListItem.jsx
+++ webapp/src/main/webapp/WEB-INF/src/components/common/GameListItem/GameListItem.jsx
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
-
+import ImageService from "../../../services/api/imageService";
 import {
     Card, Container,
 } from 'react-bootstrap';
 
 import BacklogButton from '../../common/BacklogButton/BacklogButton';
-
 import defaultGameCover from './images/default_game_cover.png';
 
 class GameListItem extends Component {
     state = {
         id: this.props.id,
         game: this.props.game,
+        cover: defaultGameCover
     };
+    
+	componentWillMount() {
+      if(this.state.game.cover == null) 
+        this.setState({ cover: defaultGameCover, }); 
+      else 
+        ImageService.getImageLink(this.state.game.cover) 
+              .then((data) => { 
+                  this.setState({ 
+                      cover: data 
+                  }); 
+              }).then((data) =>  {console.log(this.state.game.cover)}); 
+    }
     
     render() {
         return (
-            <Card bg="light-grey" text="white" style={{
-                width: '18rem',
-                paddingLeft: 10,
-                paddingRight: 10,
-                paddingTop: 10,
-                paddingBottom: 10}}>
+            <Card className="m-3 d-flex bg-transparent" style={{width: '250px',}}>
                 <BacklogButton/>
-                <Card.Img variant="top" src={this.state.game.cover == null ? defaultGameCover : this.state.game.cover} />
-                    <Container bg="primary">
-                        {this.state.game.title}
-                    </Container>
+                <a className="d-flex flex-column flex-grow-1 text-white" href={'/games/' + this.state.id}>
+	                <Card.Img className="cover" variant="top" src={this.state.cover} />
+			        <div className="card-body bg-primary flex-grow-1">
+			            <h5>{this.state.game.title}</h5>
+			        </div>
+			    </a>
             </Card>
         )
     }
