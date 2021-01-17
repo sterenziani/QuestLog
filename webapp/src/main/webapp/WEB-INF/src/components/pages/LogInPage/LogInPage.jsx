ab096ff4e020e96744e26d1c1c1c5c9d6b51d694
Paula Oseroff <poseroff@itba.edu.ar>
2021-01-17 15:06:28 -0300

    Add LogIn capabilities to LogInPage

--- webapp/src/main/webapp/WEB-INF/src/components/pages/LogInPage/LogInPage.jsx
+++ webapp/src/main/webapp/WEB-INF/src/components/pages/LogInPage/LogInPage.jsx
@@ -13,6 +13,9 @@ import * as Yup from 'yup';
 import FormikTextField from '../../common/Forms/FormikTextField';
 import AuthForm from '../../common/Forms/AuthForm';
 import AnyButton from '../../common/AnyButton/AnyButton';
+import AuthService from '../../../services/api/authService';
+import withRedirect from '../../hoc/withRedirect';
+import { OK, UNAUTHORIZED } from '../../../services/api/apiConstants';
 
 const LogInSchema = Yup.object().shape({
     username : Yup
@@ -24,12 +27,41 @@ const LogInSchema = Yup.object().shape({
 })
 
 class LogInPage extends Component {
-    state = { }
-    onSubmit = (values, { setSubmitting }) => {
-        console.log(values);
-        setSubmitting(false);
+    state = {
+        correct        : true,
+        bad_connection : false
     }
-    render() { 
+    authenticate = async (values, setSubmitting) => {
+        const { status } = await AuthService.logIn(values.username, values.password)
+
+        console.log(status);
+
+        switch(status){
+
+            case OK:
+                this.props.activateRedirect()
+                break;
+
+            case UNAUTHORIZED:
+                this.setState({
+                    correct : false
+                })
+                break;
+
+            default:
+                setSubmitting(false)
+                this.setState({
+                    bad_connection : true
+                })
+                break;
+        }
+
+    }
+    onSubmit = (values, { setSubmitting }) => {
+        setSubmitting(true);
+        this.authenticate(values, setSubmitting);
+    }
+    render() {
         return ( 
             <Formik
                 initialValues = {{
@@ -53,6 +85,26 @@ class LogInPage extends Component {
                     titleKey="login.title"
                     onSubmit={ handleSubmit }
                 >
+                    { 
+                        !this.state.correct &&
+                            <p className="form-error">
+                                <Translation>
+                                {
+                                    t => t("login.error")
+                                }
+                                </Translation>
+                            </p>
+                    }
+                    { 
+                        this.state.bad_connection &&
+                            <p className="form-error">
+                                <Translation>
+                                {
+                                    t => t("login.bad_connection")
+                                }
+                                </Translation>
+                            </p>
+                    }
                     <FormikTextField 
                         label="login.username.label"
                         name="username"
@@ -64,6 +116,7 @@ class LogInPage extends Component {
                         onBlur={ handleBlur } 
                     />
                     <FormikTextField 
+                        type="password"
                         label="login.password.label"
                         name="password"
                         placeholder="login.password.placeholder"
@@ -101,4 +154,4 @@ class LogInPage extends Component {
     }
 }
 
-export default LogInPage;
+export default withRedirect(LogInPage, "/");
